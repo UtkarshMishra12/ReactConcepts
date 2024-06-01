1aafcb3f04fe8b6ce17ed0560f3028b991ed2cd8
Utkarsh Mishra <nasautkarsh@gmail.com>
2024-06-01 18:09:28 +0530

    Map Function Concepts
    Signed-off-by: Utkarsh Mishra <nasautkarsh@gmail.com>

---
+++ MapFunction/src/components/Tours.js
@@ -0,0 +1,23 @@
+import Card from "./Card";
+
+function Tours({tours, removeTour}){
+    return (
+      <div className="container">
+
+        <div>
+          <h2 className="title">Plan with Utkash</h2>
+        </div>
+
+        <div className="cards">
+          {
+            tours.map( (tour) => {
+                return <Card {...tour} key={tour.id} removeTour={removeTour}></Card>
+            })
+          }
+        </div>
+
+      </div>
+    );
+}
+
+export default Tours;
